fbc9bcf7f343e85eed19a9c56bd86d945365e755
Lalu Arfi Maulana Pangistu <48820379+arfimaulana@users.noreply.github.com>
2019-04-06 11:40:34 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+</script>
+<body>
+	<header class="header">
+		<h1>RuangBelajar</h1>
+	</header>
+	<div class="login">
+			<h1>Login</h1>
+			
+				<div class="textbox">
+					<i class="fas fa-user"></i>
+					<input type="text" placeholder="Username" name="" value=""></input>
+				</div>
+
+				<div class="textbox">
+					<i class="fas fa-lock"></i>
+					<input type="password" placeholder="Password" name="password"/>
+				</div>			
+				<div>
+					<input type="submit" value="Login" class="btn">
+				</div>
+	</div>
+
+</body>
+</html>
