251d95c46de995f6cae01dfe07d4238f3a370892
Ganjar <ganjar1700018057@webmail.uad.ac.id>
2019-05-09 21:46:57 +0700

    Project

--- user/control/tampildata.php
+++ user/control/tampildata.php
@@ -1,16 +1,17 @@
 	<?php 
 		include '../database/config.php';
 		$email = $_SESSION['email'];
-		$data = mysqli_query($koneksi, "select matapelajaran.NamaMapel, pilihmapel.kelas from matapelajaran join pilihmapel
-			on matapelajaran.Id = pilihmapel.id where pilihmapel.email = '$email'");				
+		$data = mysqli_query($koneksi, "select matapelajaran.NamaMapel, pilihmapel.kelas, pilihmapel.Status, pilihmapel.Biaya from matapelajaran join pilihmapel
+			on matapelajaran.Id = pilihmapel.id where pilihmapel.email = '$email' AND pilihmapel.Status= 'Pending'");				
 	 ?>
    <?php 
     if(mysqli_num_rows($data)>0)
     { 
+      $totalbiaya=0;
       ?>
       <div class="row">
                     <div class="col">
-                      <h5 style="margin: 10px 0px 10px 0px; padding: 10px 0px">Daftar Kelas yang diikuti</h5>
+                      <h5 style="margin: 10px 0px 10px 0px; padding: 10px 0px">Daftar Kelas yang baru diikuti</h5>
                     </div>
                   </div>
                   <div class="row">
@@ -21,7 +22,8 @@
             <tr class="table-primary">
               <th scope="col">Mata Pelajaran</th>
               <th scope="col">Kelas</th>
-              <th scope="col">Delete</th>
+              <th scope="col">Status</th>
+              <th scope="col">Biaya</th>
             </tr>
           </thead>
           <tbody>
@@ -31,11 +33,21 @@
             <tr>
               <td><?php echo $d['NamaMapel']; ?></td>
               <td><?php echo $d['kelas']; ?></td>
-              <td><button class="btn btn-danger"><i class="far fa-trash-alt"></i></button></td>
+              <td><p style="color: red;font-weight: bold"><?php echo $d['Status']; ?></p></td>
+              <td><?php echo $d['Biaya']; ?></td>
             </tr>
-          <?php }
+
+          <?php 
+          
+          $totalbiaya += $d['Biaya'];
+           }
           ?>
+          <tr>
+            <td colspan="3">Total Biaya</td>
+            <td><?= $totalbiaya ?></td>
+          </tr>
           </tbody>
+        </table>
                               </div>
                   </div>
     <?php 
