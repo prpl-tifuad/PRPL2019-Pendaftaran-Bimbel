739f0ab443d832c490219bb1a1d040b6d1f43a48
Musa Fitriyadi <muhammad1700018055@webmail.uad.ac.id>
2019-06-26 13:05:32 +0700

    Update htaccess dan Page 404

--- user/pilihpaket/index.php
+++ user/pilihpaket/index.php
@@ -1,6 +1,22 @@
-<?php 
-include '../control/session.php';
- ?>
+<?php
+session_start();
+if ( !isset($_SESSION['email'])){
+
+	header('location:../../login/');
+	exit();
+}
+else if($_SESSION['level'] == "user"){
+	$nama = $_SESSION['nama'];
+	$email = $_SESSION['email'];
+	$password = $_SESSION['password'];
+	$no_hp = $_SESSION['no_hp'];
+	$status = $_SESSION['status'];
+	$foto = $_SESSION['fotoprofile'];
+}else{
+	header('location:../admin/');
+	exit();
+}
+?>
 <html>
 	<title>Form Data Diri</title> 
 	<link rel="stylesheet" type="text/css" href="../../assets/bootstrap/css/bootstrap.min.css">
